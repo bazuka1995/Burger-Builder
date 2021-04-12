5506a36fe2c04bd35710642e50c4c15fc6d788c7
Dov Royal <dov.royal@gmail.com>
2021-04-12 12:51:03 +1000

    Improve Performance
    Make sure the order summary isnt re-rendered when it isnt visible in the DOM.

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,30 +1,32 @@
-import React from "react";
+import React, { Component } from "react";
 import Aux from "../../../hoc/Auxilliary";
 import Button from "../../UI/Button/Button";
 
-const OrderSummary = (props) => {
-  const ingredientSummary = Object.keys(props.ingredientSummary).map(
-    (igKey) => {
-      return (
-        <li key={igKey}>
-          <span style={{ textTransform: "capitalize" }}>{igKey}</span>:{" "}
-          {props.ingredientSummary[igKey]}
-        </li>
-      );
-    }
-  );
+class OrderSummary extends Component {
+  render() {
+    const ingredientSummary = Object.keys(this.props.ingredientSummary).map(
+      (igKey) => {
+        return (
+          <li key={igKey}>
+            <span style={{ textTransform: "capitalize" }}>{igKey}</span>:{" "}
+            {this.props.ingredientSummary[igKey]}
+          </li>
+        );
+      }
+    );
 
-  return (
-    <Aux>
-      <h3>Your Order</h3>
-      <p>Your delicious burger has the following ingredients: </p>
-      <ul>{ingredientSummary}</ul>
-      <h4>Total price: ${props.price.toFixed(2)}</h4>
-      <p>Continue to payment?</p>
-      <Button btnType='Danger' click={props.cancelled}>Cancel</Button>
-      <Button btnType='Success' click={props.continue}>Continue</Button>
-    </Aux>
-  );
+    return (
+      <Aux>
+        <h3>Your Order</h3>
+        <p>Your delicious burger has the following ingredients: </p>
+        <ul>{ingredientSummary}</ul>
+        <h4>Total price: ${this.props.price.toFixed(2)}</h4>
+        <p>Continue to payment?</p>
+        <Button btnType='Danger' click={this.props.cancelled}>Cancel</Button>
+        <Button btnType='Success' click={this.props.continue}>Continue</Button>
+      </Aux>
+    );
+  }
 };
 
 export default OrderSummary;
