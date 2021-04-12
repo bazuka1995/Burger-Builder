1f96eee13a2c977c32e235d3007ca91c35844f7c
Dov Royal <dov.royal@gmail.com>
2021-04-12 12:25:23 +1000

    Add sidedrawer
    Added a sidedrawer to the mobile view.
    Make sure the toolbar menu items arent shown when viewing on a mobile device.

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,15 +1,33 @@
-import React from "react";
+import React, { Component } from "react";
 import Aux from "../../hoc/Auxilliary";
-import classes from './Layout.module.css';
-import Toolbar from '../Navigation/Toolbar/Toolbar';
+import classes from "./Layout.module.css";
+import Toolbar from "../Navigation/Toolbar/Toolbar";
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 
-const Layout = (props) => (
-  <Aux>
-    <Toolbar />
-    <main className={ classes.Content }>
-        {props.children}
-    </main>
-  </Aux>
-);
+class Layout extends Component {
+  state = {
+    showSideDrawer: false
+  }
+
+  SideDrawerClosedHandler = () => {
+    this.setState({showSideDrawer: false});
+  }
+
+  sideDrawerToggleHandler = () => {
+    this.setState((prevState) => {
+      return {showSideDrawer: !prevState.showSideDrawer};
+    });
+  }
+
+  render() {
+    return (
+      <Aux>
+        <Toolbar toggle={this.sideDrawerToggleHandler} />
+        <SideDrawer open={this.state.showSideDrawer} closed={this.SideDrawerClosedHandler} />
+        <main className={classes.Content}>{this.props.children}</main>
+      </Aux>
+    );
+  }
+}
 
 export default Layout;
