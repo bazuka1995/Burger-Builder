86075232d48958921d3ba2eb56ea8ae6d1492b25
Dov Royal <dov.royal@gmail.com>
2021-04-22 15:13:19 +1000

    Route Protection
    Make sure that the user cannot get to protected pages when not logged in (ie /orders)

--- src/App.js
+++ src/App.js
@@ -2,40 +2,59 @@ import React, { Component } from "react";
 import Layout from "./components/Layout/Layout";
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 import Checkout from "./containers/Checkout/Checkout";
-import { Route, Switch, withRouter } from "react-router-dom";
-import Orders from './containers/Orders/Orders';
-import Auth from './containers/Auth/Auth';
-import Logout from './containers/Auth/Logout/Logout'
-import { connect } from 'react-redux';
-import * as actions from './store/actions/index'
+import { Route, Switch, withRouter, Redirect } from "react-router-dom";
+import Orders from "./containers/Orders/Orders";
+import Auth from "./containers/Auth/Auth";
+import Logout from "./containers/Auth/Logout/Logout";
+import { connect } from "react-redux";
+import * as actions from "./store/actions/index";
 
 class App extends Component {
-
-  componentDidMount () {
+  componentDidMount() {
     this.props.onTrySignup();
   }
 
   render() {
+    let routes = (
+      <switch>
+        <Route path="/auth" component={Auth} />
+        <Route path="/" exact component={BurgerBuilder} />
+        <Redirect to="/" />
+      </switch>
+    );
+
+    if (this.props.isAuth) {
+      routes = (
+        <switch>
+          <Route path="/checkout" component={Checkout} />
+          <Route path="/orders" component={Orders} />
+          <Route path="/logout" component={Logout} />
+          <Route path="/" exact component={BurgerBuilder} />
+          <Redirect to="/" />
+        </switch>
+      );
+    }
+
     return (
       <div>
         <Layout>
-          <Switch>
-            <Route path="/checkout" component={Checkout} />
-            <Route path="/orders" component={Orders} />
-            <Route path="/auth" component={Auth} />
-            <Route path="/logout" component={Logout} />
-            <Route path="/" exact component={BurgerBuilder} />
-          </Switch>
+          <Switch>{routes}</Switch>
         </Layout>
       </div>
     );
   }
 }
 
-const mapDispatchToProps = dispatch => {
+const mapStateToProps = (state) => {
   return {
-    onTrySignup: () => dispatch(actions.authCheckState())
-  }
-}
+    isAuth: state.auth.token !== null,
+  };
+};
 
-export default withRouter(connect(null, mapDispatchToProps)(App));
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onTrySignup: () => dispatch(actions.authCheckState()),
+  };
+};
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
